9ebf3fe84d652729728ecf50d7f250b7d184bd91
GabrielLima1995 <gabriel-souzalima@live.com>
2021-09-03 11:52:15 -0300

    plot bokeh plot line added

--- plot_bokeh.py
+++ plot_bokeh.py
@@ -52,3 +52,51 @@ def geo_circle(geodataframe ,title,w ,h ,hovertool=None, cmp_colum = None ,palet
         p.circle(source=circles,size=circle_size, color=circle_color, alpha=alpha)
 
     return p
+
+
+def geo_line(geodataframe ,title,w ,h ,hovertool=None, cmp_colum = None ,palette_name = None,
+               line_alpha=None,line_width=None,line_color=None,cmp = False,ax =False,tile = True,
+               cmp_scale = None,cmp_min=None,cmp_max=None):
+
+    if tile: 
+      tile = {'url':'https://tiles.basemaps.cartocdn.com/dark_all/{Z}/{X}/{Y}@2x.png'}
+    else:
+      tile ={}
+    
+    from bokeh.plotting import figure
+    from bokeh.models import GeoJSONDataSource,ColumnDataSource,ColorBar,\
+    BasicTicker,WMTSTileSource
+
+    lines = GeoJSONDataSource(geojson=geodataframe.to_json())
+    tile_options = WMTSTileSource(**tile)
+    p = figure(title=title,plot_width=w, plot_height=h,tooltips=hovertool)
+    p.add_tile(tile_options)
+    p.axis.visible = ax
+    
+    if cmp:
+        
+        if cmp_min is None:
+            cmp_min = geodataframe[cmp_colum].min()
+        
+        if cmp_max is None:
+            cmp_max = geodataframe[cmp_colum].max()
+
+        if cmp_scale == 'linear':
+            from bokeh.models import LinearColorMapper
+            color_mapper = LinearColorMapper(palette = palette_name , low = cmp_min,
+                                             high = cmp_max)
+        else:
+            from bokeh.models import LogColorMapper
+            color_mapper = LogColorMapper(palette = palette_name , low = cmp_min,
+                                             high = cmp_max)
+            
+        color_bar = ColorBar(color_mapper=color_mapper, ticker=BasicTicker(),location=(0,0),
+                                 label_standoff=15)
+        p.add_layout(color_bar, 'left')
+        p.MultiLine(source=lines,line_width=line_width, line_color={'field' :cmp_colum, 'transform': color_mapper},\
+                    alpha=line_alpha)
+    else :
+        p.MultiLine(source=lines,line_width=line_width, line_color=line_color, line_alpha=line_alpha)
+
+    return p
+
