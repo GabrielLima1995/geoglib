943f84fb00c1721d5611af7feabe389b8d11fe33
GabrielLima1995 <gabriel-souzalima@live.com>
2021-09-11 16:58:14 -0300

    Added new functions in gdf_manipulation module

--- gpd_manipulation.py
+++ gpd_manipulation.py
@@ -4,3 +4,7 @@ def create_geopandas(dataframe,geometry_columns,in_crs,out_crs):
     g.crs = in_crs
     g = g.to_crs(out_crs)
     return g
+
+def distance_in_meters(x, y):
+    from geopy.distance import vincenty
+    return vincenty((x[0], x[1]), (y[0], y[1])).m
